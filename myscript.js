ea9fd4fa9ee1490941bb214fe16a60d78fa5dbe1
Evy04 <sanya.satish2020@vitbhopal.ac.in>
2022-12-06 03:14:21 +0530

    pushing basic script

---
+++ myscript.js
@@ -0,0 +1,11 @@
+$(window).on('load',function() {
+    $('.flexslider').flexslider(
+        {
+            animation: "slide",
+            slideshowSpedd: 2000,
+            direction: "vertical",
+            reverse: true,
+            pauseOnHover: true
+        }
+    );
+});
